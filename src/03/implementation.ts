27283831c2361d7acbf94f09e5176cd5e0e02489
Penners <penry.downey@gmail.com>
2024-12-13 17:39:12 +0000

    day 3 part 2

--- src/03/implementation.ts
+++ src/03/implementation.ts
@@ -1,13 +1,41 @@
+const innerRegex = /mul\((?<l>\d{1,3}),(?<r>\d{1,3})\)/g;
+const outerRegex = /(?:do\(\)|^)(?<valid>.*?)(?:don't\(\)|$)/g;
+
 export const part1 = (input: string) => {
-  const regex = /mul\((?<l>[0-9]+),(?<r>[0-9]+)\)/g;
   let m;
   let output = 0;
-  while ((m = regex.exec(input)) !== null) {
-    if (m.index === regex.lastIndex) {
-      regex.lastIndex++;
+  while ((m = innerRegex.exec(input)) !== null) {
+    if (m.index === innerRegex.lastIndex) {
+      innerRegex.lastIndex++;
     }
     if (m.groups?.l && m.groups?.r) {
       output += parseInt(m.groups?.l) * parseInt(m.groups?.r);
+    }
+  }
+  return output;
+};
+
+export const part2 = (input: string) => {
+  let m;
+  let output = 0;
+  const joined = input.replace(/(\r\n?|\n|\t)/g, " ");
+
+  while ((m = outerRegex.exec(joined)) !== null) {
+    if (m.index === outerRegex.lastIndex) {
+      outerRegex.lastIndex++;
+    }
+
+    if (m.groups?.valid) {
+      let b;
+      while ((b = innerRegex.exec(m.groups?.valid)) !== null) {
+        if (b.index === innerRegex.lastIndex) {
+          innerRegex.lastIndex++;
+        }
+        if (b.groups?.l && b.groups?.r) {
+          const res = parseInt(b.groups?.l) * parseInt(b.groups?.r);
+          output += res;
+        }
+      }
     }
   }
   return output;
--- src/03/test.test.ts
+++ src/03/test.test.ts
@@ -1,13 +1,15 @@
 import { describe, expect, it } from "vitest";
 import fs from "fs";
 import path from "path";
-import { part1 } from "./implementation";
+import { part1, part2 } from "./implementation";
 
 const example = fs.readFileSync(path.join(__dirname, "example.txt")).toString();
-
+const example2 = fs
+  .readFileSync(path.join(__dirname, "example.2.txt"))
+  .toString();
 const input = fs.readFileSync(path.join(__dirname, "input.txt")).toString();
 
-describe("day 2 part 1", () => {
+describe("day 3 part 1", () => {
   it("example works", () => {
     const result = part1(example);
     expect(result).toBe(161);
@@ -19,14 +21,14 @@ describe("day 2 part 1", () => {
   });
 });
 
-// describe("day 2 part 2", () => {
-//   it("example works", () => {
-//     const result = part2(example);
-//     expect(result).toBe(4);
-//   });
+describe("day 3 part 2", () => {
+  it("example works", () => {
+    const result = part2(example2);
+    expect(result).toBe(48);
+  });
 
-//   it("real implementation works", () => {
-//     const result = part2(input);
-//     expect(result).toBe(349);
-//   });
-// });
+  it("real implementation works", () => {
+    const result = part2(input);
+    expect(result).toBe(89349241);
+  });
+});
